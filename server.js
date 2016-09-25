5a639305be36fe10595e0b82e27fd2ff8ac248f5
Jessica Wilson <jessicalostinspace@gmail.com>
2016-09-24 18:18:44 -0700

    intialized server on port 8000

---
+++ server.js
@@ -0,0 +1,20 @@
+// require express
+var express = require("express");
+var httpsRedirect = require('express-https-redirect');
+var path = require("path");
+var app = express();
+
+app.use(express.static(path.join(__dirname, "./views")));
+
+// // setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+
+app.set('view engine', 'html');
+
+// tell the express app to listen on port 8000
+var server = app.listen(8000, function() {
+	console.log("🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀");
+	console.log("🚀 Starship blast-off on port 8000 🚀");
+	console.log("🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀");
+})
+
